967b8bba283993025c945e64b8641b4d873eeb12
IenVos <annadelapierre@icloud.com>
2026-03-06 16:59:47 +0100

    Nieuwe functies toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Suspense } from "react";
+import Script from "next/script";
 import "./globals.css";
 import { ConvexClientProvider } from "@/lib/ConvexClientProvider";
 import { AboutModalProvider } from "@/lib/AboutModalContext";
@@ -75,6 +76,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="nl">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','422608397394430');fbq('track','PageView');`}
+        </Script>
+      </head>
       <body>
         <ErrorBoundary>
           <ConvexClientProvider>
