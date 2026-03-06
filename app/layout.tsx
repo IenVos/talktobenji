d56abe8375ec75d43ef4256a5805f85db7e6cf89
IenVos <annadelapierre@icloud.com>
2026-03-06 17:59:15 +0100

    Nieuwe functies toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init','422608397394430');fbq('track','PageView');`}
+fbq('init','1207409694491677');fbq('track','PageView');`}
         </Script>
       </head>
       <body>
