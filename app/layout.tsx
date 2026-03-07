f814dc1c21d9968974a8feed1bfcf8317107500b
IenVos <annadelapierre@icloud.com>
2026-03-07 14:08:03 +0100

    Nieuwe functies toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
-import { Suspense } from "react";
 import Script from "next/script";
+import { Suspense } from "react";
 import "./globals.css";
 import { ConvexClientProvider } from "@/lib/ConvexClientProvider";
 import { AboutModalProvider } from "@/lib/AboutModalContext";
@@ -77,15 +77,23 @@ export default function RootLayout({
   return (
     <html lang="nl">
       <head>
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2054062258501361&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
+      <body>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init','1255091969361681');fbq('track','PageView');`}
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','2054062258501361');fbq('track','PageView');`}
         </Script>
-      </head>
-      <body>
         <ErrorBoundary>
           <ConvexClientProvider>
             <SessionProvider>
