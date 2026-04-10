dbf9b0b75045950c304f37e5963e8dff31098d33
IenVos <annadelapierre@icloud.com>
2026-04-10 18:22:47 +0200

    Nieuwe functies toegevoegd 

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,15 @@ export default function RootLayout({
   return (
     <html lang="nl">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LJWMLNK');`,
+          }}
+        />
         <noscript>
           <img height="1" width="1" style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=2054062258501361&ev=PageView&noscript=1"
@@ -122,6 +131,14 @@ export default function RootLayout({
         />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5LJWMLNK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-QBDGHJYXM0" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || [];
